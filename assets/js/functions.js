2d2a4aec79cbebed6e89df42c2eadc7ff88ad842
Cristobal Dominguez <cristobald@me.com>
2016-02-08 19:33:26 -0300

    Commit Inicial

---
+++ assets/js/functions.js
@@ -0,0 +1,19 @@
+// remap jQuery to $
+(function($){})(window.jQuery);
+
+
+/* trigger when page is ready */
+$(document).ready(function (){
+
+	// your functions go here
+
+});
+
+
+
+
+/* optional triggers */
+
+$(window).resize(function() {
+	
+});
